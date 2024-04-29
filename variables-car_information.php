5a4dca3f5aa0c6dff6f67f4921895c0151a6aff9
DamianTocev <84261493+DamianTocev@users.noreply.github.com>
2024-04-29 12:36:00 +0200

    Add files via upload

---
+++ variables-car_information.php
@@ -0,0 +1,8 @@
+<?php
+$car = "Volvo";
+$horse_powers = 122.5;
+
+echo "<h1>This is my car information:</h1>";
+echo "<p>Make: " . $car . "</p>";
+echo "<p>Horse powers: " . $horse_powers . "</p>";
+?>
